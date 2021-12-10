e466b4b49f44161af723d0eb36b82b85fa286607
mehdieft <50948075+mehdieft@users.noreply.github.com>
2021-12-10 21:26:13 +0330

    adding microsoft entity framework dsign v6

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,21 +19,21 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
-        }
+            _config = config;
+        } 
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlite("conection string");
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 
-            });ds
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
