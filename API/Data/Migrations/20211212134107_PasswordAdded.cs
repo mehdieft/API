cbf763ea8a527021402adf379ecc8ae320ef2377
mehdieft <50948075+mehdieft@users.noreply.github.com>
2021-12-12 17:16:12 +0330

    adding password and saltpassword which is idont know what its
    adding to column to user table
    migrate it
    update the database

---
+++ API/Data/Migrations/20211212134107_PasswordAdded.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Password",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "passwordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "passwordSalt",
+                table: "Users");
+        }
+    }
+}
