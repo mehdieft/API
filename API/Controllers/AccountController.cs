bdb74fed79c9b14b06b363ad4c3e68b9c446d805
mehdieft <50948075+mehdieft@users.noreply.github.com>
2021-12-13 06:57:58 +0330

    creating example of resource thats not use full for us

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+
+
+    public class AccountController: ControllerBase
+    {
+    private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+        //[HttpPost]
+        //you have to naming inside this shit
+        [HttpPost("register")]
+        public ActionResult<AppUser> Register([FormBody] )
+        {
+            return request
+            // using var hmac =new HMACSHA512();
+            // var user = new AppUser
+            // {
+            //     UserName = username,
+            //     Password = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            //     passwordSalt=hmac.Key
+                
+            // };
+            // _context.Users.Add(user);
+            // _context.SaveChanges();
+            // return user;
+        }
+       
+
+    }
+}
