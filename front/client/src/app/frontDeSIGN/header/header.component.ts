bdb5624bb7d024e7691cef1d3827faac092a1c56
mehdieft <50948075+mehdieft@users.noreply.github.com>
2021-12-14 11:44:58 +0330

    adding toaste to nesseccery parts

--- front/client/src/app/frontDeSIGN/header/header.component.ts
+++ front/client/src/app/frontDeSIGN/header/header.component.ts
@@ -3,6 +3,8 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 import { Observable } from 'rxjs';
 import {AccountServiceService} from '../../services/account-service.service';
 import {User} from '../../interfaces/user';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-header',
@@ -16,24 +18,45 @@ export class HeaderComponent implements OnInit {
 
   registerUser(){
     const dialog = this.dialog.open(RegisterDialog,{width: '95%', minHeight: '20vh', maxWidth: '75vw', closeOnNavigation: true});
+    dialog.afterClosed().subscribe(registerResponse=>{
+      console.log("this is register response",registerResponse)
+      this.accountService.register(registerResponse).subscribe(res=>{
+        this.toastr.success("register",'register successfully');
+
+      
+      },(err=>{
+        console.log("error",err)
+        this.toastr.error('error','pls check your fills');
+      })
+      )
+      
+
+    })
   }
   loginUser(){
    let dg= this.dialog.open(LoginDialog,{width: '95%', minHeight: '20vh', maxWidth: '75vw', closeOnNavigation: true});
    dg.afterClosed().subscribe(resLogin=>{
      console.log("res login",resLogin);
      this.accountService.login(resLogin).subscribe(response=>{
+       this.toastr.success('success','your register succesfully');
       console.log("res----",response);
+
     },err=>{
+      this.toastr.error('error','somethings wrong with login')
       console.log("error",err)
     }
     )
    })
   }
   logoutUser(){
+    this.toastr.success('success','your logout successfully')
     this.accountService.logout();
   }
 
-  constructor(private dialog:MatDialog,private accountService: AccountServiceService) { }
+  constructor(private dialog:MatDialog,
+    private accountService: AccountServiceService,
+    private toastr: ToastrService,
+    ) { }
 
   ngOnInit(): void {
     this.currentUser$=this.accountService.currentUser$;
@@ -45,13 +68,15 @@ export class HeaderComponent implements OnInit {
   templateUrl: './registerDialog.component.html',
 })
 export class RegisterDialog {
-  constructor(private accountService: AccountServiceService){}
+  constructor(private accountService: AccountServiceService,private dialogRef: MatDialogRef<RegisterDialog>){}
    model:any={}
    
   register(){
     console.log("this is model ",this.model)
+      this.dialogRef.close(this.model);
         this.accountService.register(this.model).subscribe(res=>{
           console.log("this is resault",res)
+          
         },err=>{
           console.log("error is happaned",err);
         }
