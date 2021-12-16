a996e1518bb78c57be725ceefb924be376190f96
mehdieft <50948075+mehdieft@users.noreply.github.com>
2021-12-17 02:20:21 +0330

    adding more column to the User database a
    adding more column to the database
    skip the seeding
    adding relationship
    Learning about repository in the the this toturial

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Entities
+﻿using API.Extensions;
+
+namespace API.Entities
 {
     public class AppUser
     {
@@ -6,6 +8,24 @@
         public string UserName { get; set; }
         public byte[] Password { get; set; }
         public byte[] passwordSalt { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime BirthOfDate { get; set; }
+
+        public DateTime Created { get; set; }= DateTime.Now;
+        public DateTime LastActive { get; set; }= DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> photoes {get;set;} 
+
+        public int GetAge()
+        {
+            return BirthOfDate.CalculateAge();
+        }
 
     }
 }
+
