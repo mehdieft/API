ca4a3a63ade10e469563b7ae49bbb115831369fb
mehdieft <50948075+mehdieft@users.noreply.github.com>
2021-12-17 12:04:12 +0330

    idont know whats going on

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        bool saveAll();
+
+        IEnumerable<AppUser> GetUsers();
+        AppUser GetById(int id);
+
+        AppUser GetByUsename(string id);
+    }
+}
---
+++ API/Repository/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using API.Data;
+using API.Entities;
+using API.Interfaces;
+using System.Data.Entity;
+using System.Linq;
+
+namespace API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public AppUser GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public AppUser GetByUsename(string id)
+        {
+            return _context.Users.SingleOrDefault(x => x.UserName == id);
+        }
+
+        public IEnumerable<AppUser> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public bool saveAll()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = (Microsoft.EntityFrameworkCore.EntityState)EntityState.Modified;
+        }
+    }
+}
